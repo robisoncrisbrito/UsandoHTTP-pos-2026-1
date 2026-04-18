5e2fe330cc80ecba7b25bdc04694256c4c4f1582
Robison Cris Brito <44240574+robisoncrisbrito@users.noreply.github.com>
2026-04-18 11:31:45 -0300

    Aplicativo já utilizando o LocationManager para recuperar a posição do GPS

--- app/src/main/java/br/edu/utfpr/usandohttp/MainActivity.kt
+++ app/src/main/java/br/edu/utfpr/usandohttp/MainActivity.kt
@@ -1,5 +1,9 @@
 package br.edu.utfpr.usandohttp
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
 import android.os.UserManager
@@ -8,10 +12,11 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), LocationListener {
 
     private lateinit var tvLatitude: TextView
     private lateinit var tvLongitude: TextView
@@ -37,9 +42,34 @@ class MainActivity : AppCompatActivity() {
 
         locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
 
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, this)
+
     }
 
     fun btBuscarEnderecoOnClick(view: View) {
 
+    }
+
+    override fun onLocationChanged(location: Location) {
+        tvLatitude.text = location.altitude.toString()
+        tvLongitude.text = location.longitude.toString()
     }
 }
